dcb05a651f5303970809fe32fddd0a9f251cfed7
Maria Agatha Sia <sia.mb@obmontessori.edu.ph>
2026-03-12 19:40:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function showRegularFare() {
-}
-
-function showDiscountedFare() {
+// Fare Matrix Discount
+let regluarFare = 50; // Base fare in pesos
+function getRegularFare() {
+    
+    document.getElementById("square-regular").innerHTML = `
+        <div class="row justify-content-center mt-3">
+            <div class="col-10 bg-dark text-white p-3"> 
+                <h5>Fare Details:</h5>
+                <h4>Regular Fare:<span class="badge bg-success">₱${regularFare.toFixed(2)}</span></h4>
+            </div>
+        </div>
+    `;
 }
